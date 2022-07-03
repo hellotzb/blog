26c552d102ace8c779ecf04b13fdec52f6af2af7
hellotzb <1328887499@qq.com>
2022-07-03 16:16:35 +0800

    feat: SSR渲染文章列表
    --story=20220703

--- pages/api/index.d.ts
+++ pages/api/index.d.ts
@@ -1,3 +1,14 @@
 import { IronSession } from 'iron-session';
+import { IUserInfo } from 'store/user';
 
 export type ISession = IronSession & Record<string, any>;
+
+export type IArticle = {
+  id: number;
+  title: string;
+  content: string;
+  create_time: Date;
+  update_time: Date;
+  views: number;
+  user: IUserInfo;
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,47 @@
 import type { NextPage } from 'next';
+import { GetServerSideProps } from 'next';
+import { AppDataSource } from '@/db/data-source';
+import { Article } from '@/db/entity';
+import ListItem from '@/components/ListItem';
+import { IArticle } from './api';
+import { Divider } from 'antd';
 
-const Home: NextPage = () => {
-  return <div>首页</div>;
+import styles from './index.module.scss';
+
+interface IProps {
+  articles: IArticle[];
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  if (!AppDataSource.isInitialized) {
+    await AppDataSource.initialize();
+  }
+  const articles = await AppDataSource.getRepository(Article).find({
+    relations: {
+      user: true, // 返回值关联users表
+    },
+  });
+
+  // console.log('server-articles', articles);
+
+  return {
+    props: { articles: JSON.parse(JSON.stringify(articles)) || [] },
+  };
+};
+
+const Home: NextPage<IProps> = (props) => {
+  const { articles = [] } = props;
+  console.log('client-articles', articles);
+  return (
+    <div className={styles['content-layout']}>
+      {articles?.map((article) => (
+        <div key={article.id}>
+          <ListItem article={article} />
+          <Divider />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
